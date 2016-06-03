fcde73e306ac62433163f7e70a63d1809bd33578
tim70036 <tim70036@gmail.com>
2016-06-03 22:12:15 +0800

    Nothing

--- src/Client/PictureSheet.java
+++ src/Client/PictureSheet.java
@@ -5,12 +5,12 @@ import processing.core.PImage;
 public class PictureSheet {
 	private PImage img;
 	
-	public PictureSheet(PImage pic){
-		try{
+	public PictureSheet(PImage pic)
+	{
+		try
+		{
 			this.img = pic;
-		}catch(Exception e){
-			e.printStackTrace();
-		}
+		}catch(Exception e){e.printStackTrace();}
 	}
 	
 	public PImage getPicture(int x,int y ){
